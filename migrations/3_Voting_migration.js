09fb10a2c9f9718482bfa3956fc88e95bbb35f4d
cromi <darionalis@gmail.com>
2023-02-03 20:55:53 +0100

    voting-migration added

--- migrations/3_Voting_migration.js
+++ migrations/3_Voting_migration.js
@@ -1,9 +1,12 @@
+const fs = require('fs');
+const registerContract = JSON.parse(fs.readFileSync('../build/contracts/Register.json', 'utf8'));
+
 const Voting = artifacts.require("VotingContract");
 const theQuestion = web3.utils.asciiToHex("what is superior?");
 const candidateNames = [web3.utils.asciiToHex("chocolate"), web3.utils.asciiToHex("vanilla")]
-const RegistrationContractAddress = "0x05eaB455e5F32e0b087c351101062e3F4e8417F7";
+const registrationContractAddress = registerContract.networks[5].address;
 const endElectionTime = 1675431565;
 
 module.exports = function (deployer) {
-	deployer.deploy(Voting,theQuestion, candidateNames, RegistrationContractAddress,endElectionTime);
+	deployer.deploy(Voting,theQuestion, candidateNames, registrationContractAddress,endElectionTime);
 };
